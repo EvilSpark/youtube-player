55873dec387291677651858281ca3eb45b3a53bf
EvilSpark <kamrantahir25@gmail.com>
2019-07-22 01:05:36 +0530

    Styles remaining

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import SearchBar from './components/search_bar';
+import YTsearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyCQ5bTQ_34RCmaYIgD8sJDpkh6-VfR7tog';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('React js');
+	}
+
+	videoSearch(searchTerm) {
+		YTsearch({key: API_KEY, term: searchTerm}, data => {
+			console.log(data);
+			this.setState({
+				videos: data,
+				selectedVideo: data[0]
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div className="main">
+				<div className="search">
+					<SearchBar
+						onSearchTermChange={searchTerm => this.videoSearch(searchTerm)}
+					/>
+				</div>
+				<div className="video-printed">
+					<VideoDetail video={this.state.selectedVideo} />
+					<VideoList
+						onVideoSelect={userSelected =>
+							this.setState({selectedVideo: userSelected})
+						}
+						videos={this.state.videos}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default App;
+//export default {App};
